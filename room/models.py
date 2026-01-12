abf77d70651075dd028c96a6f3302eb5835b699e
Andrii Yermolenko <vakt159@gmail.com>
2026-01-12 22:49:20 +0200

    Create model Room

--- room/models.py
+++ room/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    class RoomType(models.TextChoices):
+        SINGLE = "Single"
+        DOUBLE = "Double"
+        SUITE = "Suite"
+    number = models.CharField(max_length=255, unique=True)
+    type = models.CharField(choices=RoomType)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    capacity = models.IntegerField()
