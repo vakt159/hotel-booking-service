b2c3f16d25b99f3be864dfb4d537b745b3adb922
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-14 22:14:52 +0200

    fix: change BookingViewSet from ReadOnly to ModelViewSet to support create action

--- booking/views.py
+++ booking/views.py
@@ -1,14 +1,18 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import ReadOnlyModelViewSet
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from booking.filters import BookingFilter
 from booking.models import Booking
-from booking.serializers import BookingReadSerializer
+from booking.serializers import (
+    BookingReadSerializer,
+    BookingCreateSerializer
+)
 
 
-class BookingViewSet(ReadOnlyModelViewSet):
-    queryset = Booking.objects.select_related("room", "user")
+class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingReadSerializer
+    permission_classes = [IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_class = BookingFilter
 
@@ -20,4 +24,19 @@ class BookingViewSet(ReadOnlyModelViewSet):
 
         return queryset.filter(user=self.request.user)
 
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BookingCreateSerializer
+        return BookingReadSerializer
 
+    def create(self, request, *args, **kwargs):
+        """Create new booking with user auto-attachment and price from room."""
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        booking = serializer.save()
+
+        response_serializer = BookingReadSerializer(booking)
+        return Response(
+            response_serializer.data,
+            status=status.HTTP_201_CREATED
+        )
