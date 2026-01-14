4c313ee227c5e2d031afbd13ef5f3c6335240185
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-14 21:37:47 +0200

    feat: add filter for booking list

---
+++ booking/filters.py
@@ -0,0 +1,19 @@
+import django_filters
+from booking.models import Booking
+
+
+class BookingFilter(django_filters.FilterSet):
+    from_date = django_filters.DateFilter(
+        field_name="check_in_date", lookup_expr="gte"
+    )
+    to_date = django_filters.DateFilter(
+        field_name="check_out_date", lookup_expr="lte"
+    )
+
+    room_type = django_filters.CharFilter(
+        field_name="room__type", lookup_expr="iexact"
+    )
+
+    class Meta:
+        model = Booking
+        fields = ["user", "room", "status"]
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from booking.filters import BookingFilter
 from booking.models import Booking
 from booking.serializers import BookingReadSerializer
 
@@ -6,4 +9,6 @@ from booking.serializers import BookingReadSerializer
 class BookingViewSet(ReadOnlyModelViewSet):
     queryset = Booking.objects.select_related("room", "user")
     serializer_class = BookingReadSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BookingFilter
 
