889d4a5803ee02499d0fc21114803370ad4a93bc
VYGAKOOL <vygakool@gmail.com>
2026-01-16 20:46:01 +0200

    Update stripe_service.py

--- payment/services/stripe_service.py
+++ payment/services/stripe_service.py
@@ -1,28 +1,29 @@
 import stripe
 from django.conf import settings
 
-from payment.models import Payment
-
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-def create_checkout_session(payment: Payment, success_url: str, cancel_url: str):
+
+def create_checkout_session(payment,success_url, cancel_url):
     session = stripe.checkout.Session.create(
-        mode="payment",
         payment_method_types=["card"],
+        mode="payment",
+        success_url=success_url,
+        cancel_url=cancel_url,
         line_items=[{
             "price_data": {
                 "currency": "usd",
-                "product_data": {"name": f"Booking #{payment.booking.id}"},
+                "product_data": {
+                    "name": f"Booking #{payment.booking.id} | Room {payment.booking.room.number}",
+                },
                 "unit_amount": int(payment.money_to_pay * 100),
             },
             "quantity": 1,
         }],
-        success_url=success_url,
-        cancel_url=cancel_url,
     )
 
     payment.session_id = session.id
     payment.session_url = session.url
-    payment.save()
+    payment.save(update_fields=["session_id", "session_url"])
 
-    return session.url
+    return payment
